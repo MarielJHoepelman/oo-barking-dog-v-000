bdaa486c60c1a5d44c9ca8f66ae39e3acbc757fc
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-01-12 18:57:40 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
+class Dog
+
+ def initialize(name, bark)
+    @name = name
+    @bark = bark
+  end
+
+  def name=(name)
+    @name
+  end
+
+  def bark=(bark)
+    @bark puts "Woof"
+  end
+
+  def bark=(bark)
+    @bark
+  end
+end
 # Your code goes here!
